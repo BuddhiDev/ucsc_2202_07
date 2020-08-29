46290a7ed1e2ecbd695bf78a037f506d78d96f49
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-08-29 16:07:28 +0530

    Create edit_profile.php

---
+++ andum.lk/tailor/edit_profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>andum.lk</title>
+    <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/loginstyle.css">
+    <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
+</head>
+<body>
+  <div class="box">
+    <header>
+
+
+      <img class="logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
+      <nav>
+        <ul class="nav-area">
+          <li><a href="#">Explore</a></li>
+          <li><a href="#">Hire a Fashion Designer</a></li>
+          <li><a href="#">Dress Showcase</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+      </nav>
+      <div>
+
+  </body>
+  </html>
