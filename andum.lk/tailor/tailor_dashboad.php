f70be06d46b86c512086235d962bf4c2eb617159
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-08-29 13:39:33 +0530

    Update tailor_dashboad.php

--- andum.lk/tailor/tailor_dashboad.php
+++ andum.lk/tailor/tailor_dashboad.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>andum.lk</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
 </head>
 <body>
     <div class="box">
       <header>
-        <img class="logo" src="logo.png" alt="logo">
+        <img class="logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
         <nav>
           <ul class="nav-area">
             <li><a href="#">Explore</a></li>
             <li><a href="#">Hire a Fashion Designer</a></li>
             <li><a href="#"></a>Dress Showcase</li>
-            <li><a href="/ucsc_2202_07/andum.lk/Manage_order.php">Manage order</a></li>
+            <li><a href="/ucsc_2202_07/andum.lk/tailor/Manage_order.php">Manage order</a></li>
           </ul>
         </nav>
         <div>
