703997b42afc8fbcbf862c846449079d67a6bb53
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-08-29 13:07:39 +0530

    Create Manage_order.php

---
+++ andum.lk/Manage_order.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>andum.lk</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+      table {
+        border-collapse: collapse;
+        border-spacing: 0;
+        width: 100%;
+        border: 1px solid #ddd;
+      }
+
+      th, td {
+        text-align: left;
+        padding: 8px;
+      }
+
+      tr:nth-child(even){background-color: #f2f2f2}
+    </style>
+</head>
+<body>
+    <div class="box">
+      <header>
+        <img class="logo" src="logo.png" alt="logo">
+        <nav>
+          <ul class="nav-area">
+            <li><a href="#">Explore</a></li>
+            <li><a href="#">Hire a Fashion Designer</a></li>
+            <li><a href="#"></a>Dress Showcase</li>
+          </ul>
+        </nav>
+        <div>
+          <a class="cta" href="#"><button>Sign Out</button></a>
+        </div>
+      </header>
+    </div>
+    <div style="overflow-x:auto;">
+    <table>
+      <tr>
+        <th>Order Id</th>
+        <th>NIC</th>
+        <th>Customer Name</th>
+        <th>Start Date</th>
+        <th>Status</th>
+        <th>Complete Date</th>
+        <th>Edit Details</th>
+      </tr>
+      <tr>
+      <td>1001</td>
+      <td>965233243v</td>
+      <td>Smith</td>
+      <td>2020-08-03</td>
+      <td>on going</td>
+      <td>2020-08-30</td>
+      <td>
+        <div>
+          <a class="cta" href="#"><button>Edit</button></a>
+        </div>
+      </td>
+    </tr>
+
+    </table>
+  </div>
+
+</body>
+</html>
