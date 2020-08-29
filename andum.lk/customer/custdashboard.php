f61584f508cecc63f61eb77e66d631f3d73e583d
Thashwini <thashwiniwattuhewa@gmail.com>
2020-08-29 15:46:48 +0530

    committed

---
+++ andum.lk/customer/custdashboard.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>andum.lk</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="box">
+        <header>
+          <img class="logo" src="logo.png" alt="logo">
+          <nav>
+            <ul class="nav-area">
+              <li><a href="#">Explore</a></li>
+              <li><a href="#">Hire a Tailor</a></li>
+              <li><a href="#">Hire a Fashion Designer</a></li>
+              <li><a href="#">About Us</a></li>
+            </ul>  
+          </nav>
+          <div>
+            <a class="cta" href="/ucsc_2202_07/andum.lk/login.php"><button>Sign Out</button></a>
+          </div>
+        </header>
+      </div>
+    <!-- <div class="wrapper">
+        <aside class="bg-gra-200 border-r border-gray-300">Left</aside>
+        <main>Right</main> 
+    </div> -->
+    <div class="search-container">
+    <div class="form-field-inline">
+        <label for="uname" class="field-label-inline">Search by category</label>
+        <input type="text" class="field-value-inline" name="uname" required>
+    </div>
+    </div>
+
+    <div class="btn-panel-center">
+            <!-- <button class="facebookbutton btn-full-w" type="submit">SIGN UP WITH FACEBOOK </button>
+            <button class="googlebutton btn-full-w" type="submit">SIGN UP WITH GOOGLE </button> -->
+            <button class="loginbutton btn-full-w" type="submit">All</button>
+    </div>
+    
+    <!-- <section class="products">
+        <div class="product-container">
+            <h1>ALL</h1>
+            <div class="row">
+                <div class="col-md-3">
+                     <div class="img-box"></div>
+                     <img src="product1.jpg" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <!-- <div class="card">
+  <img src="product1.jpg" alt="Denim Jeans" style="width:100%">
+  <h1>Tailored Jeans</h1> -->
+  <!-- <p class="price">$19.99</p>
+  <p>Some text about the jeans..</p>
+  <p><button>Add to Cart</button></p> -->
+<!-- </div> -->
+
+<div class="slider-box">
+    <p class="time">New</p>
+    <div class="img-box">
+        <img src="product1.jpg" alt="">
+    </div>
+    <p class="detail">Black Ladies Bags Collections
+        <a href="#" class="price">Price</a>
+    </p>
+    <div class="cart">
+        <a href="#">Add To Cart</a>
+    </div>
+</div>
+
+
+    
+</body>
+</html>
