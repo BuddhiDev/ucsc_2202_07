53adab3fba0d15b0e7f4c25d236a3f305fbd9757
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-08-29 14:55:34 +0530

    Update tailor_dashboad.php

--- andum.lk/tailor/tailor_dashboad.php
+++ andum.lk/tailor/tailor_dashboad.php
@@ -5,8 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>andum.lk</title>
     <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
+    <style>
+        .card {
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+          max-width: 300px;
+          margin: auto;
+          text-align: center;
+          font-family: arial;
+        }
+
+        .title {
+          color: grey;
+          font-size: 18px;
+        }
+      </style>
 </head>
 <body>
+  <!--create nav bar -->
     <div class="box">
       <header>
         <img class="logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
@@ -23,5 +38,15 @@
         </div>
       </header>
     </div>
+    <!--profile-->
+    <div class="card">
+  <img src="/ucsc_2202_07/andum.lk/tailor/tailor-01.jpg" alt="John" style="width:100%">
+  <h1>Jeny Doe</h1>
+  <p class="title">Tailor</p>
+  <p>Colombo</p>
+  <p>Jeny@mail.com</p>
+  <p>1224435534<p>
+
+</div>
 </body>
 </html>
