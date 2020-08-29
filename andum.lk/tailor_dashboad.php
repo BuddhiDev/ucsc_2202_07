8a7ef50d9c084909121bf5d2458e9607d9bd4751
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-08-29 13:07:15 +0530

    Create tailor_dashboad.php

---
+++ andum.lk/tailor_dashboad.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>andum.lk</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="box">
+      <header>
+        <img class="logo" src="logo.png" alt="logo">
+        <nav>
+          <ul class="nav-area">
+            <li><a href="#">Explore</a></li>
+            <li><a href="#">Hire a Fashion Designer</a></li>
+            <li><a href="#"></a>Dress Showcase</li>
+            <li><a href="/ucsc_2202_07/andum.lk/Manage_order.php">Manage order</a></li>
+          </ul>
+        </nav>
+        <div>
+          <a class="cta" href="#"><button>Sign Out</button></a>
+        </div>
+      </header>
+    </div>
+</body>
+</html>
