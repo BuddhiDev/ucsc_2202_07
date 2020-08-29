b48c22ec93db88cfad8881a145a05f0cee977fe6
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-08-29 13:11:32 +0530

    btr

--- andum.lk/customer_dashboard.php
+++ andum.lk/customer_dashboard.php
@@ -5,10 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>andum.lk</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 </head>
 <body>
     <div class="box">
+    <header>
 
-    </div>
+
+            <img class="logo" src="logo.png" alt="logo">
+            <nav>
+                <ul class="nav-area">
+                    <li><a href="#">Explore</a></li>
+                    <li><a href="#">Hired Tailors</a></li>
+                    <li><a href="#">Hired Fashion Designer</a></li>
+                    <li><a href="#">My orders</a></li>
+                </ul>
+            </nav>
+            <!-- <div>
+                <a class="cta" href="/ucsc_2202_07/andum.lk/login.php"><button>Sign In</button></a>
+                <a class="cta" href="/ucsc_2202_07/andum.lk/signup.php"><button>Sign Up</button></a>
+
+            </div> -->
+
+        </div>
+
+
+
+    </header>
+</div>
+<center>
+<div class="main">
+  <div class="columns">
+    <div class ="row">
+      <div class="content">
+        <img src="tailor1.jfif" alt="Avatar" style="width:200px">
+        <h4><b>Manel Fernando</b></h4>
+        <p>Tailor</p>
+
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+
+        <div>
+          <a class="cta" href="/ucsc_2202_07/andum.lk/hire.php"><button>Hire</button></a>
+
+        </div>
+
+  </div>
+  </div>
+</div>
+</div>
+</center>
 </body>
 </html>
--- andum.lk/index.php
+++ andum.lk/index.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>andum.lk</title>
     <link rel="stylesheet" href="style.css">
-    
+
 </head>
 <body>
     <div class="box">
     <header>
-        
-        
+
+
             <img class="logo" src="logo.png" alt="logo">
             <nav>
                 <ul class="nav-area">
@@ -19,14 +19,15 @@
                     <li><a href="#">Hire a Tailor</a></li>
                     <li><a href="#">Hire a Fashion Designer</a></li>
                     <li><a href="#">About Us</a></li>
-                </ul>  
+                </ul>
             </nav>
             <div>
                 <a class="cta" href="/ucsc_2202_07/andum.lk/login.php"><button>Sign In</button></a>
                 <a class="cta" href="/ucsc_2202_07/andum.lk/signup.php"><button>Sign Up</button></a>
+              
             </div>
-       
-        
+
+
     </header>
 </div>
 </body>
