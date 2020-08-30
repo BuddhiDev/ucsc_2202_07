a0c7bff7a24975ebe851ae67b41c244e69381188
Thashwini <thashwiniwattuhewa@gmail.com>
2020-08-30 08:51:17 +0530

    committed

--- andum.lk/index.php
+++ andum.lk/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>andum.lk</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/loginstyle.css">
 
 
 
