3a2bdf12994f9bb82a53e9b3c6a903ae65cd3507
Thashwini <thashwiniwattuhewa@gmail.com>
2020-08-30 15:26:47 +0530

    commit

--- andum.lk/customer/index.php
+++ andum.lk/customer/index.php
@@ -70,8 +70,8 @@ if(!isset($_SESSION['nic'])){
 			   while ($row = mysqli_fetch_assoc($result)) {
         		$t_nic = $row["nic"];
             ?>
-            <div slider-box>
-            <form method="post" action="index.php">
+            <div class="slider-box">
+            <form method="post" action="index.php" class="tailor-form">
         <img src="../tailor1.jfif" alt="Avatar" style="width:200px">
         <h4><b>
          <input type="hidden" value="<?php echo $row["nic"] ?> " name="t_nic">
@@ -110,5 +110,37 @@ if(!isset($_SESSION['nic'])){
 </div>
 </div> -->
 </center>
+
+<!--footer-->
+<div class="footer">
+    <div class="footer-content">
+      <div class="footer-section about">
+        <img class="footer-logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo"><h1 class="logo-txt"><span></span></h1>
+        <div class="socials">
+          <a href="#"><i class="fas fa-facebook"></i></a>
+          <a href="#"><i class="fas fa-instagram"></i></a>
+          <a href="#"><i class="fas fa- google+"></i></a>
+        </div>
+      </div>
+      <div class="footer-section links">
+        <ul>
+          <a href="#"><li>HIRE A TAILOR</li></a>
+          <a href="#"><li>HIRE A FASHION DESIGNER</li></a>
+          <a href="#"><li>SHIPPING AND RETURNS</li></a>
+        </ul>
+      </div>
+      <div class="footer-section links2 ">
+        <ul>
+          <a href="#"><li>HIRE A TAILOR</li></a>
+          <a href="#"><li>HIRE A FASHION DESIGNER</li></a>
+          <a href="#"><li>SHIPPING AND RETURNS</li></a>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-bottom">
+          &copy; andum.lk
+    </div>
+  </div>
+  
 </body>
 </html>
