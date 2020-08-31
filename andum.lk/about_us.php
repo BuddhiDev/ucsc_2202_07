bc93a2ecbf1a05bc45a76c4127f0a0aaeb58fe8d
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-08-31 08:01:31 +0530

    Create about_us.php

---
+++ andum.lk/about_us.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>andum.lk</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/loginstyle.css">
+</head>
+<body>
+  <div class="box">
+    <header>
+      <img class="logo" src="logo.png" alt="logo">
+      <nav>
+        <ul class="nav-area">
+            <li><a href="#">Explore</a></li>
+            <li><a href="#">Hire a Tailor</a></li>
+            <li><a href="#">Hire a Fashion Designer</a></li>
+            <li><a href="#">About Us</a></li>
+        </ul>
+      </nav>
+      <div>
+        <a class="cta" href="/ucsc_2202_07/andum.lk/login.php"><button class="loginbutton btn-full-w">Sign In</button></a>
+        <a class="cta" href="/ucsc_2202_07/andum.lk/signup.php"><button class="loginbutton btn-full-w">Sign Up</button></a>
+      </div>
+    </header>
+  </div>
+
+<!-- add profiles -->
+
+<center>  <div class="about-section">
+    <h1>Contact Us Today</h1>
+    <p>Some text about who we are and what we do.</p>
+    <p>Resize the browser window to see that this page is responsive by the way.</p>
+  </div>
+  <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.3379327488956!2d80.53273831471529!3d5.948084995691325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae13f28dd68ba3d%3A0x388afcae4e289914!2sNupe%20Junction!5e0!3m2!1sen!2slk!4v1598840582857!5m2!1sen!2slk" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></p>
+</center>
+  <h2 style="text-align:center">Our Team</h2>
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+        <div class="container">
+          <h2>Jane Doe</h2>
+          <p class="title">CEO & Founder</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>jane@example.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="/w3images/team2.jpg" alt="Mike" style="width:100%">
+        <div class="container">
+          <h2>Mike Ross</h2>
+          <p class="title">Art Director</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>mike@example.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="/w3images/team3.jpg" alt="John" style="width:100%">
+        <div class="container">
+          <h2>John Doe</h2>
+          <p class="title">Designer</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>john@example.com</p>
+          <p><button class="button">Contact</button></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
