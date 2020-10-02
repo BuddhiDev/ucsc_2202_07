f991a235425454607849a1ecb064a4e9ce8b9f18
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-10-02 14:42:26 +0530

    Update add_product.php

--- andum.lk/tailor/add_product.php
+++ andum.lk/tailor/add_product.php
@@ -22,27 +22,6 @@
   padding: 8px 10px;
 }
 
-/* Set a style for the submit button */
-button {
-    padding: 9px 25px;
-    background-color:#EB2188;
-    border: none;
-    border-radius: 50px;
-    cursor: pointer;
-    transition: all 0.3s ease 0s;
-    text-align: center;
-}
-
-.button {
-    display: flex 2;
-    background-color:#F5F3FF;
-    color: black;
-}
-
-button:hover {
-    color: black;
-}
-
 </style>
 
 </head>
@@ -68,9 +47,9 @@ button:hover {
 
 
 <!--add new product form-->
-<form action="/action_page.php" style="max-width:800px;margin:auto">
 
-    <div class="input-container">
+
+  <form action="/action_page.php" style="max-width:800px;margin:auto">
       <h2>Add Product</h2>
 
       <div class="row">
@@ -94,49 +73,56 @@ button:hover {
 
       <div class="row">
         <div class="col-25">
-        <label for="dname" class="field-label-inline"><b>Dress Name</b></label>
+          <label for="dname" class="field-label-inline"><b>Dress Name</b></label>
+        </div>
+        <div class="col-75">
+          <input type="text" class="field-value-inline" placeholder="Name" name="dname">
+        </div>
       </div>
-      <div class="col-75">
-        <input type="text" class="field-value-inline" placeholder="Name" name="dname">
-      </div>
-    </div>
 
 
 
     <div class="row">
       <div class="col-25">
-      <lable for="price" class="field-label-inline">Price: </label>
+        <lable for="price" class="field-label-inline">Price: </label>
+      </div>
+      <div class="col-75">
+        <input class="field-label-inline" type="text"  placeholder="Price" name="price">
+      </div>
     </div>
-    <div class="col-75">
-      <input class="field-label-inline" type="text"  placeholder="Price" name="price">
-    </div>
-  </div>
 
-  <div class="row">
-    <div class="col-25">
-      <lable for="type" class="field-label-inline">Size:</label>
+    <div class="row">
+      <div class="col-25">
+        <lable for="type" class="field-label-inline">Size:</label>
+      </div>
+      <div class="col-75">
+        <select class="option" name="Size" id="size">
+          <option value="1">M</option>
+          <option value="2">L</option>
+          <option value="3">XL</option>
+          <option value="4">XXL</option>
+          <option value="5">XXXL</option>
+        </select>
+      </div>
     </div>
-    <div class="col-75">
-      <select class="option" name="Size" id="size">
-        <option value="1">M</option>
-        <option value="2">L</option>
-        <option value="3">XL</option>
-        <option value="4">XXL</option>
-        <option value="5">XXXL</option>
-    </select>
+
+    <div class="row">
+      <div class="col-25">
+        <lable for="type" class="field-label-inline">Upload Dress image:</label>
+      </div>
+      <div class="col-75">
+        <input type="file" id="myFile" name="filename">
+        <input type="submit">
+      </div>
     </div>
-  </div>
-
-  </div>
- </form>
-
+  </form>
 
 
 <!--footer
   <div class="footer">
     <div class="footer-content">
       <div class="footer-section about">
-        <img class="footer-logo" src="logo.png" alt="logo"><h1 class="logo-txt"><span></span></h1>
+        <img class="footer-logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo"><h1 class="logo-txt"><span></span></h1>
         <div class="socials">
           <a href="#"><i class="fas fa-facebook"></i></a>
           <a href="#"><i class="fas fa-instagram"></i></a>
