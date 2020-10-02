2fd190d3ce6e9175f4c3cf73257cc531e0e827ad
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-10-03 00:52:55 +0530

    Update signup.php

--- andum.lk/signup.php
+++ andum.lk/signup.php
@@ -10,6 +10,7 @@ require("server.php");
     <title>Andum.lk - Sign Up</title>
     <link rel="stylesheet" href="loginstyle.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="dropdown.css">
     <style>
     .option{
       background-color: #EB2188;
@@ -25,12 +26,49 @@ require("server.php");
 
 
       <img class="logo" src="logo.png" alt="logo">
+      <div class="navbardrop">
+                      <div class="dropdown">
+                        <button class="dropbtn">Categories
+                        <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                          <div class="row">
+                            <div class="column">
+                              <h3>Ladies wear</h3>
+                              <a href="#">Blouses</a>
+                              <a href="#">Skirts</a>
+                              <a href="#">Tops</a>
+                              <a href="#">Frocks</a>
+                              <a href="#">Shorts</a>
+                              <a href="#">Trouses</a>
+                              <a href="#">T-Shirts</a>
+                              <a href="#">Party Frocks</a>
+                              <a href="#">Sarees</a>
+                              <a href="#">Bridal Dress</a>
+                              <a href="#"></a>
+                            </div>
+                            <div class="column">
+                              <h3>Gents wear</h3>
+                              <a href="#">Shirts</a>
+                              <a href="#">Trousers</a>
+                              <a href="#">Denims</a>
+                              <a href="#">Shorts</a>
+                              <a href="#">T-shirts</a>
+                              <a href="#">Groom Dress</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
       <nav>
         <ul class="nav-area">
+          
           <li><a href="#">Explore</a></li>
           <li><a href="#">Hire a Tailor</a></li>
           <li><a href="#">Hire a Fashion Designer</a></li>
           <li><a href="#">About Us</a></li>
+
         </ul>
       </nav>
       <div>
