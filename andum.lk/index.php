99770ec9bd290aae4188ccdee65102e5f809cda0
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-10-02 22:40:03 +0530

    Update index.php

--- andum.lk/index.php
+++ andum.lk/index.php
@@ -14,7 +14,7 @@
       <nav>
         <ul class="nav-area">
             <li><a href="#">Explore</a></li>
-            <li><a href="#">Hire a Tailor</a></li>
+            <li><a href="/ucsc_2202_07/andum.lk/unregistered_user_hire_tailor.php">Hire a Tailor</a></li>
             <li><a href="#">Hire a Fashion Designer</a></li>
             <li><a href="/ucsc_2202_07/andum.lk/about_us.php">About Us</a></li>
 
