60b65d7a087edbc3cd39ce510fbd479b0dfc885f
Thashwini <thashwiniwattuhewa@gmail.com>
2020-10-05 15:15:41 +0530

    committed

--- andum.lk/customer/measurement_form.php
+++ andum.lk/customer/measurement_form.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
 
-<style>
+<!-- <style>
 .col-25 {
   float: left;
   width: 25%;
@@ -28,9 +28,6 @@
 .input-field:focus {
   border: 2px solid black;
 }
-
-
-
 
 .option{
   background-color: #EB2188;
@@ -59,18 +56,15 @@ button:hover {
     color: black;
 }
 
-</style>
+</style> -->
 
 </head>
 
 <body>
 
-
   <!--nav bar-->
   <div class="box">
     <header>
-
-
       <img class="logo" src="logo.png" alt="logo">
       <nav>
         <ul class="nav-area">
@@ -81,55 +75,55 @@ button:hover {
           <li><a href="/ucsc_2202_07/andum.lk/about_us.php">About Us</a></li>
         </ul>
       </nav>
-
     </header>
   </div>
 
-
 <!--measurement form-->
-<div class="input-container">
-  <form action="/action_page.php" style="max-width:500px;margin:auto">
-
-    <h2>Measurement Form</h2>
-
-    <div class="row">
-      <div class="col-25">
-        <lable for="type" class="input-field">Category:</label>
-        <img src="" class="image">
-      </div>
-      <div class="col-75">
-        <select class="option" name="Unit" id="unit">
-          <optgroup label="Ladies wear">
-            <option value="1">Blouse</option>
-            <option value="2">skirt</option>
-            <option value="3">frock</option>
-            <option value="4">Short</option>
-            <option value="5">Trouser</option>
-            <option value="6">T-Shirt</option>
-            <option value="7">Party Frock</option>
-            <option value="8">Saree</option>
-            <option value="9">Bridal Dress</option>
-          </optgroup>
-          <optgroup label="Gents wear">
-            <option value="10">Shirt</option>
-            <option value="11">T-shirt</option>
-            <option value="12">Denim</option>
-            <option value="13">Trouser</option>
-            <option value="14">Groom Dress</option>
-          </optgroup>
-          <optgroup label="Kids wear">
-            <option value="15">School Uni Forms</option>
-            <option value="16">T-shirt</option>
-            <option value="17">Trouser</option>
-            <option value="18">Frocks</option>
-            <option value="19">skirts</option>
-          </optgroup>
-        </select>
-    </div>
-   </div>
+  <div class="input-container">
+    <form action="/action_page.php" style="max-width:500px;margin:auto">
+    <div class="measuer">
+      <h2>Get Measured</h2>
+      <div class="row">
+        <div class="col-25">
+          <lable for="type" class="input-field">Category:</label>
+          <img src="" class="image">
+        </div>
+        <div class="col-75">
+          <select class="option" name="Unit" id="unit">
+            <optgroup label="Ladies wear">
+              <option value="1">Blouse</option>
+              <option value="2">skirt</option>
+              <option value="3">frock</option>
+              <option value="4">Short</option>
+              <option value="5">Trouser</option>
+              <option value="6">T-Shirt</option>
+              <option value="7">Party Frock</option>
+              <option value="8">Saree</option>
+              <option value="9">Bridal Dress</option>
+            </optgroup>
+            <optgroup label="Gents wear">
+              <option value="10">Shirt</option>
+              <option value="11">T-shirt</option>
+              <option value="12">Denim</option>
+              <option value="13">Trouser</option>
+              <option value="14">Groom Dress</option>
+            </optgroup>
+            <optgroup label="Kids wear">
+              <option value="15">School Uni Forms</option>
+              <option value="16">T-shirt</option>
+              <option value="17">Trouser</option>
+              <option value="18">Frocks</option>
+              <option value="19">skirts</option>
+            </optgroup>
+          </select>
+        </div>
+  </div>
 
   <div>
     <div class="row">
+      <div>
+
+      </div>
       <div class="col-25">
         <lable for="neck" class="input-field">Neck:</label>
         <img src="" class="image">
@@ -193,7 +187,7 @@ button:hover {
       <textarea name="message" rows="20" cols="50"></textarea>
     </div>
 	  <button type="submit" class="btn">Submit</button>
-
+    </div>
   </form>
 </div>
 
