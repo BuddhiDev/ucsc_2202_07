364c30b17d16db18316ebcbc241e85b7fc5964b7
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-10-05 23:18:05 +0530

    Update login.php

--- andum.lk/login.php
+++ andum.lk/login.php
@@ -17,52 +17,7 @@ require("server.php");
   <div class="box">
     <header>
       <img class="logo" src="logo.png" alt="logo">
-      <div class="navbardrop">
-            <div class="dropdown">
-            <button class="dropbtn">Categories
-                  <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <div class="row">
-                      <div class="column">
-                              <h3>Ladies wear</h3>
-                              <a href="#">Blouses</a>
-                              <a href="#">Skirts</a>
-                              <a href="#">Tops</a>
-                              <a href="#">Frocks</a>
-                              <a href="#">Shorts</a>
-                              <a href="#">Trouses</a>
-                              <a href="#">T-Shirts</a>
-                              <a href="#">Party Frocks</a>
-                              <a href="#">Sarees</a>
-                              <a href="#">Bridal Dresses</a>
-
-                      </div>
-                      <div class="column">
-                              <h3>Gents wear</h3>
-                              <a href="#">Shirts</a>
-                              <a href="#">Trousers</a>
-                              <a href="#">Denims</a>
-                              <a href="#">Shorts</a>
-                              <a href="#">T-shirts</a>
-                              <a href="#">Groom Dresses</a>
-                      </div>
-
-                      <div class="column">
-                              <h3>Casual wear</h3>
-                              <a href="#">Casual Wear blouse</a>
-                              <a href="#">Casual wear pants</a>
-                              <a href="#">Casual wear Denims</a>
-                              <a href="#">Casual wearShorts</a>
-                              <a href="#">casual wear T-shirts</a>
-
-                      </div>
-
-
-                  </div>
-              </div>
-           </div>
-        </div>
+      <?php require("navbar_category.php")?>
       <nav>
         <ul class="nav-area">
           <li><a href="#">Explore</a></li>
