03ba0c97dd1f27c280a9df4c5640f85e4e2079fb
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-10-05 23:17:21 +0530

    Update index.php

--- andum.lk/index.php
+++ andum.lk/index.php
@@ -12,48 +12,7 @@
   <div class="box">
     <header>
       <img class="logo" src="logo.png" alt="logo">
-      <div class="navbardrop">
-            <div class="dropdown">
-            <button class="dropbtn">Categories
-                  <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                <div class="row">
-                      <div class="column">
-                              <h3>Ladies wear</h3>
-                              <a href="#">Blouses</a>
-                              <a href="#">Skirts</a>
-                              <a href="#">Tops</a>
-                              <a href="#">Frocks</a>
-                              <a href="#">Shorts</a>
-                              <a href="#">Trouses</a>
-                              <a href="#">T-Shirts</a>
-                              <a href="#">Party Frocks</a>
-                              <a href="#">Sarees</a>
-                              <a href="#">Bridal Dresses</a>
-                              <a href="#"></a>
-                      </div>
-                      <div class="column">
-                              <h3>Gents wear</h3>
-                              <a href="#">Shirts</a>
-                              <a href="#">Trousers</a>
-                              <a href="#">Denims</a>
-                              <a href="#">Shorts</a>
-                              <a href="#">T-shirts</a>
-                              <a href="#">Groom Dresses</a>
-                      </div>
-                      <div class="column">
-                              <h3>Kids wear</h3>
-                              <a href="#">School Uni Forms</a>
-                              <a href="#">T-shirt</a>
-                              <a href="#">Trousers</a>
-                              <a href="#">Frocks</a>
-                              <a href="#">skirts</a>
-                      </div>
-                  </div>
-              </div>
-           </div>
-        </div>
+      <?php require("navbar_category.php")?>
       <nav>
         <ul class="nav-area">
             <li><a href="/ucsc_2202_07/andum.lk/explore.php">Explore</a></li>
