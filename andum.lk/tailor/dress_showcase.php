e3a608edb6978af29f94bbe713c37585713f905c
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-10-05 13:19:11 +0530

    Update dress_showcase.php

--- andum.lk/tailor/dress_showcase.php
+++ andum.lk/tailor/dress_showcase.php
@@ -24,9 +24,9 @@ if (!isset($_SESSION['nic'])) {
       <img class="logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
       <nav>
         <ul class="nav-area">
-          <li><a href="index.php">Home</a></li>
+          <li><a href="/ucsc_2202_07/andum.lk/index.php">Home</a></li>
           <li><a href="dress_showcase.php">Dress Showcase</a></li>
-          <li><a href="Manage_order.php">Manage Orders</a></li>
+          <li><a href="/ucsc_2202_07/andum.lk/tailor/Manage_order.php">Manage Orders</a></li>
           <li><a href="#">Manage Sales</a></li>
         </ul>
       </nav>
