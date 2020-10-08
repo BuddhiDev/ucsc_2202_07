08026cd7297f7c4bf7ed246ab30e4de414de8a83
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-10-08 12:01:37 +0530

    ss

---
+++ andum.lk/fashion_designer/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Andum.lk - Tailor</title>
+  <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
+  <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/loginstyle.css">
+</head>
+
+<body>
+  <div class="box">
+    <header>
+      <img class="logo" src="../logo.png" alt="logo">
+      <nav>
+        <ul class="nav-area">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="dress_showcase.php">Design Showcase</a></li>
+          <li><a href="Manage_order.php">Manage Orders</a></li>
+          <li><a href="#">Manage Sales</a></li>
+        </ul>
+      </nav>
+
+      <div>
+        <a class="cta" href="edit_profile.php"><button name="edit" class="loginbutton btn-full-w">Edit profile</button></a>
+      </div>
+
+      <div>
+        <a class="cta" href="index.php?logout='1'"><button name="logout" class="loginbutton btn-full-w">Sign Out</button></a>
+      </div>
+    </header>
+  </div>
+  <?php require("footer.php")?>
+</body>
+
+</html>
