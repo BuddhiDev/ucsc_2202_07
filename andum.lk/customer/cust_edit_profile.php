fa5df9a538a36980535583eac5c5f1c22ca87c1c
BuddhiDev <39466972+BuddhiDev@users.noreply.github.com>
2020-10-13 23:09:22 +0530

    sada

--- andum.lk/customer/cust_edit_profile.php
+++ andum.lk/customer/cust_edit_profile.php
@@ -12,6 +12,8 @@ require("../server.php");
     <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/loginstyle.css">
     <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
     <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/tailor/checkbox.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://kit.fontawesome.com/dc4ee3e80e.js" crossorigin="anonymous"></script>
 
     <style>
       img {
@@ -21,19 +23,74 @@ require("../server.php");
 
 </head>
 <body>
-          <div class="box">
-            <header>
-              <img class="logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
-              <nav>
-                <ul class="nav-area">
-                  <li><a href="index.php">Home</a></li>
-                  <li><a href="#">Hire a Fashion Designer</a></li>
-                  <li><a href="dress_showcase.php">Dress Showcase</a></li>
-                  <li><a href="/ucsc_2202_07/andum.lk/about_us.php">About Us</a></li>
+
+<header>
+      <nav class="navbar-main">
+        <div class="navbar-logo">
+          <img class="logo" src="../logo.png" alt="logo" class="img-box">
+        </div>
+        <div class="nav-item-middle">
+          <ul class="nav-area">
+            <li><a href="index.php">Explore</a></li>
+            <li class="dropdown">
+              <a href="dress_showcase.php">Shop Now</a>
+              <ul class="menu-area">
+                <ul>
+                  <h4>Ladies wear</h4>
+                  <li><a href="dresses.php">Dresses</a></li>
+                  <li><a href="workware_ladies.php">Work Wear</a></li>
+                  <li><a href="#">Denims</a></li>
+                  <li><a href="#">Tops</a></li>
+                  <li><a href="#">T Shirts</a></li>
+                  <li><a href="#">All Categories</a></li>
                 </ul>
-              </nav>
-            </header>
-          <div>
+                <ul>
+                  <h4>Gents wear</h4>
+                  <li><a href="#">Shirts</a></li>
+                  <li><a href="#">Denims</a></li>
+                  <li><a href="workwear_gents.php">Work Wear</a></li>
+                  <li><a href="#">T Shirts</a></li>
+                  <li><a href="#">All Categories</a></li>
+                </ul>
+                <ul>
+                  <h4>Kids wear</h4>
+                  <li><a href="#">Dresses</a></li>
+                  <li><a href="#">T shirsts</a></li>
+                  <li><a href="#">Dresses</a></li>
+                  <li><a href="#">All Categories</a></li>
+                </ul>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="box">
+          <div class="nav_right">
+            <ul>
+              <li><i class="fas fa-user-circle"></i>
+                <div class="dd_right">
+                  <ul>
+                    <li><a href="cust_edit_profile.php"><i class="fas fa-edit"></i>Edit Profile</a></li>
+                    <li><a href="hired_list.php"><i class="fas fa-users"></i>Hired Tailors</a></li>
+                    <li><a href="#"><i class="fas fa-users"></i>Hired Fashion Designers</a></li>
+                    <li><a href="purchases.php"><i class="fas fa-money"></i>Purchases</a></li>
+                    <li><a href="#"><i class="fas fa-heart"></i>Favourites</a></li>
+                    <li><a href="index.php?logout='1'"><i class="fas fa-sign-out-alt" name="logout"></i>Sign Out</a></li>
+                  </ul>
+                </div>
+              <li class="nr_li"><i class="fas fa-envelope"></i></li>
+            </ul>   
+          </div>
+        </div>
+      </nav>
+    </header>
+
+
+<script>
+  document.querySelector(".nav_right ul li").addEventListener("click",
+  function(){
+    this.classList.toggle("active");
+  })
+</script>
 
 
 <!-- edit profile form-->
--- andum.lk/customer/purchases.php
+++ andum.lk/customer/purchases.php
@@ -15,11 +15,13 @@ if (!isset($_SESSION['nic'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/style.css">
   <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/loginstyle.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://kit.fontawesome.com/dc4ee3e80e.js" crossorigin="anonymous"></script>
   <style>
     table {
       border-collapse: collapse;
       border-spacing: 0;
-      width: 100%;
+      width: 99%;
       border: 1px solid #ddd;
     }
 
@@ -36,22 +38,75 @@ if (!isset($_SESSION['nic'])) {
 </head>
 
 <body>
-  <div class="box">
-    <header>
-      <img class="logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
-      <nav>
-        <ul class="nav-area">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="dress_showcase.php">Dress Showcase</a></li>
-          <li><a href="hired_list.php">Hired Tailors</a></li>
-          <li><a href="purchases.php">Purchases</a></li>
-        </ul>
+
+<header>
+      <nav class="navbar-main">
+        <div class="navbar-logo">
+          <img class="logo" src="../logo.png" alt="logo" class="img-box">
+        </div>
+        <div class="nav-item-middle">
+          <ul class="nav-area">
+            <li><a href="index.php">Explore</a></li>
+            <li class="dropdown">
+              <a href="dress_showcase.php">Shop Now</a>
+              <ul class="menu-area">
+                <ul>
+                  <h4>Ladies wear</h4>
+                  <li><a href="dresses.php">Dresses</a></li>
+                  <li><a href="workware_ladies.php">Work Wear</a></li>
+                  <li><a href="#">Denims</a></li>
+                  <li><a href="#">Tops</a></li>
+                  <li><a href="#">T Shirts</a></li>
+                  <li><a href="#">All Categories</a></li>
+                </ul>
+                <ul>
+                  <h4>Gents wear</h4>
+                  <li><a href="#">Shirts</a></li>
+                  <li><a href="#">Denims</a></li>
+                  <li><a href="workwear_gents.php">Work Wear</a></li>
+                  <li><a href="#">T Shirts</a></li>
+                  <li><a href="#">All Categories</a></li>
+                </ul>
+                <ul>
+                  <h4>Kids wear</h4>
+                  <li><a href="#">Dresses</a></li>
+                  <li><a href="#">T shirsts</a></li>
+                  <li><a href="#">Dresses</a></li>
+                  <li><a href="#">All Categories</a></li>
+                </ul>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="box">
+          <div class="nav_right">
+            <ul>
+              <li><i class="fas fa-user-circle"></i>
+                <div class="dd_right">
+                  <ul>
+                    <li><a href="cust_edit_profile.php"><i class="fas fa-edit"></i>Edit Profile</a></li>
+                    <li><a href="hired_list.php"><i class="fas fa-users"></i>Hired Tailors</a></li>
+                    <li><a href="#"><i class="fas fa-users"></i>Hired Fashion Designers</a></li>
+                    <li><a href="purchases.php"><i class="fas fa-money"></i>Purchases</a></li>
+                    <li><a href="#"><i class="fas fa-heart"></i>Favourites</a></li>
+                    <li><a href="index.php?logout='1'"><i class="fas fa-sign-out-alt" name="logout"></i>Sign Out</a></li>
+                  </ul>
+                </div>
+              <li class="nr_li"><i class="fas fa-envelope"></i></li>
+            </ul>   
+          </div>
+        </div>
       </nav>
-      <div>
-        <a class="cta" href="index.php?logout='1'"><button name="logout" class="loginbutton btn-full-w">Sign Out</button></a>
-      </div>
     </header>
-  </div>
+
+
+<script>
+  document.querySelector(".nav_right ul li").addEventListener("click",
+  function(){
+    this.classList.toggle("active");
+  })
+</script>
+
   <div>
     <table>
       <tr>
@@ -80,7 +135,7 @@ if (!isset($_SESSION['nic'])) {
             <td><?php echo $row["status"] ?></td>
             <td>
               <div>
-                <a class="cta" href="#"><button class="loginbutton btn-full-w">View</button></a>
+                <center><a class="cta" href="#"><button class="loginbutton btn-full-w">View</button></a></center>
               </div>
             </td>
       </tr>
