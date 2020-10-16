9d5c12518f57201ef1bf9176be3eb342fdbff435
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-10-16 11:37:57 +0530

    s

---
+++ andum.lk/fashion_designer/fd_controller.php
@@ -0,0 +1,110 @@
+<?php
+
+//session start
+session_start();
+
+//initialize global variables
+$nic = "";
+$email = "";
+$errors = array();
+$search=false;
+$category_filter=false;
+
+//connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'andum');
+
+//register
+if (isset($_POST['register'])) {
+    $nic = mysqli_real_escape_string($db, $_POST['nic']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password1 = mysqli_real_escape_string($db, $_POST['password']);
+    $password2 = mysqli_real_escape_string($db, $_POST['c_password']);
+    $fname = mysqli_real_escape_string($db, $_POST['fname']);
+    $lname = mysqli_real_escape_string($db, $_POST['lname']);
+    $contactno = mysqli_real_escape_string($db, $_POST['contactno']);
+    $utype = mysqli_real_escape_string($db, $_POST['Usertype']);
+    $addres = mysqli_real_escape_string($db, $_POST['address']);
+    $postal = mysqli_real_escape_string($db, $_POST['postal']);
+
+    if ($password1 != $password2) {
+        array_push($errors, "Passwords do not match");
+    }
+    if (count($errors) == 0) {
+        $password = md5($password1);
+        // inser to user table
+        $sql = "INSERT INTO users (nic, fname, email, lname, contactno, password, type, address, postalcode) VALUES ('$nic','$fname','$email','$lname','$contactno','$password','$utype','$addres','$postal')";
+        mysqli_query($db, $sql);
+
+        // insert user if tailor
+        if ($utype == 0) {
+            $sql1 = "INSERT INTO tailors (nic) VALUES ('$nic')";
+            mysqli_query($db, $sql1);
+        }
+
+        // insert user if customer
+        else if ($utype == 1) {
+            $sql1 = "INSERT INTO customers (nic) VALUES ('$nic')";
+            mysqli_query($db, $sql1);
+        }
+
+        //save session cache
+        $_SESSION['nic'] = $nic;
+        $_SESSION['fname'] = $fname;
+        $_SESSION['lname'] = $lname;
+        $_SESSION['success'] = "You are now logged in";
+        $_SESSION['utype'] = "$utype";
+
+        if ($_SESSION['utype'] == 0) {
+            header('location: tailor/index.php');
+        } else if ($_SESSION['utype'] == 1) {
+            header('location: customer/index.php');
+        }
+    }
+}
+
+//login
+if (isset($_POST['login'])) {
+
+    $nic = mysqli_real_escape_string($db, $_POST['nic']);
+    $password1 = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (count($errors) == 0) {
+        $password = md5($password1);
+        $sql = "SELECT * FROM users WHERE nic='$nic' AND password='$password'";
+        $result = mysqli_query($db, $sql);
+
+        if (mysqli_num_rows($result) == 1) {
+            $_SESSION['nic'] = $nic;
+            $_SESSION['success'] = "You are now logged in";
+            if ($row = mysqli_fetch_assoc($result)) {
+                $_SESSION['fname'] = $row['fname'];
+                $_SESSION['lname'] = $row['lname'];
+            }
+
+
+            $sql = "SELECT type FROM users WHERE nic='$nic' AND password='$password'";
+            $result = mysqli_query($db, $sql);
+
+            if ($row = mysqli_fetch_assoc($result)) {
+                $_SESSION['utype'] = $row['type'];
+            }
+
+
+            if ($_SESSION['utype'] == 0) {
+                header('location: tailor/index.php');
+            } else if ($_SESSION['utype'] == 1) {
+                header('location: customer/index.php');
+            }
+        } else {
+            array_push($errors, "Wrong username/ password combination");
+            //header('location: login.php');
+        }
+    }
+}
+
+//logout
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['nic']);
+    header('location: ../login.php');
+}
---
+++ andum.lk/fashion_designer/footer1.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title></title>
+    <style>
+    
+.fa {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding-top: 1px;
+    font-size: 20px;
+    width: 30px;
+    text-align: center;
+    text-decoration: none;
+    border: solid;
+    border-width: 2px;
+    border-radius: 100px;
+    border-color: white;
+    background: white;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  color: black;
+}
+
+.fa-twitter {
+  color: black;
+}
+
+.fa-google {
+  color: black;
+}
+
+.fa-linkedin {
+  color: black;
+}
+
+.fa-youtube {
+  color: black;
+}
+
+.fa-instagram {
+  color: black;
+}
+
+
+</style>
+  </head>
+  <body>
+    <div class="footer">
+      <div class="footer-content">
+        <div class="footer-section about">
+          <img class="footer-logo" src="/ucsc_2202_07/andum.lk/logo.png" alt="logo">
+
+          <h1 class="logo-txt"><span></span></h1>
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-twitter"></a>
+          <!-- <a href="#" class="fa fa-google"></a>
+          <a href="#" class="fa fa-linkedin"></a>
+          <a href="#" class="fa fa-youtube"></a> -->
+          <a href="#" class="fa fa-instagram"></a>
+        </div>
+        <div class="footer-section links">
+          <ul>
+            <a href="#">
+              <li>HIRE A TAILOR</li>
+            </a>
+            <a href="#">
+              <li>HIRE A FASHION DESIGNER</li>
+            </a>
+            <a href="#">
+              <li>SHIPPING AND RETURNS</li>
+            </a>
+          </ul>
+        </div>
+        <div class="footer-section links2 ">
+          <ul>
+            <a href="#">
+              <li>HIRE A TAILOR</li>
+            </a>
+            <a href="#">
+              <li>HIRE A FASHION DESIGNER</li>
+            </a>
+            <a href="#">
+              <li>SHIPPING AND RETURNS</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        &copy; andum.lk
+      </div>
+    </div>
+
+  </body>
+</html>
