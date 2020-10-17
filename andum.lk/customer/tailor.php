3870b12bb564e1285fbebcd103350c1740b9a4c2
BuddhiDev <39466972+BuddhiDev@users.noreply.github.com>
2020-10-17 13:39:52 +0530

    Update tailor.php

--- andum.lk/customer/tailor.php
+++ andum.lk/customer/tailor.php
@@ -325,80 +325,11 @@ if (!isset($_SESSION['nic'])) {
               <p style="color: black; font-size: 20px; margin-top:10px"><?php echo $row["fname"]." ".$row["lname"] ?></p>
               <br>
               <div>
-                <p style="color: black; font-size: 15px; margin-top:10px">Color</p>
                 <div>
-                  <a href="#">
-                    <div class="on"></div>
-                  </a>
-                  <a href="#">
-                    <div class="on1"></div>
-                  </a>
-                  <a href="#">
-                    <div class="on2">
-                      <p background-color="pink"></p>
-                    </div>
-                  </a>
-                  <a href="#">
-                    <div class="on3">
-                      <p background-color="pink"></p>
-                    </div>
-                  </a>
-                  <a href="#">
-                    <div class="on4">
-                      <p background-color="pink"></p>
-                    </div>
-                  </a>
-                  <a href="#">
-                    <div class="on5">
-                      <p background-color="pink"></p>
-                    </div>
-                  </a>
-                </div>
-                <br>
-                <div>
-                  <p style="color: black; font-size: 15px; margin-top:10px">Size</p>
-                  <div>
-                    <a href="#">
-                      <div class="onee">
-                        <p class="text">UK8</p>
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div class="onee">
-                        <p class="text">UK10</p>
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div class="onee">
-                        <p class="text">UK12</p>
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div class="onee">
-                        <p class="text">UK14</p>
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div class="onee">
-                        <p class="text">UK16</p>
-                      </div>
-                    </a>
-                    <a href="#">
-                      <div class="onee">
-                        <p class="text">UK18</p>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-                <br>
-
-                <div class="quantity">
-                  <input type="number" value="1" class="quentity-input">
-                </div>
-                <div>
+                  Please fill down below application with your dress requirements to hire me.
                   <br>
                   <br>
-                  <button class="cart-button" type="submit" name="hireT">Hire Me</button>
+                  
                 </div>
               </div>
             </form>
@@ -406,6 +337,235 @@ if (!isset($_SESSION['nic'])) {
         </div>
       </div>
     </div>
+
+
+  <!--measurement form-->
+  <div class="input-container">
+    <form action="/action_page.php" style="max-width:1024px;margin:auto">
+      <h2 class="measure-headding">Get Measured</h2>
+      <div class="d-flex">
+        <div class="f1">
+          <lable for="type" >Category:</label>
+          <img src="" class="image">
+              <!-- </div>
+              <div class="f1"> -->
+          <select class="option" name="Unit" id="unit" >
+            <optgroup label="Ladies wear">
+              <option value="1">Blouse</option>
+              <option value="2">skirt</option>
+              <option value="3">frock</option>
+              <option value="4">Short</option>
+              <option value="5">Trouser</option>
+              <option value="6">T-Shirt</option>
+              <option value="7">Party Frock</option>
+              <option value="8">Saree</option>
+              <option value="9">Bridal Dress</option>
+            </optgroup>
+            <optgroup label="Gents wear">
+              <option value="10">Shirt</option>
+              <option value="11">T-shirt</option>
+              <option value="12">Denim</option>
+              <option value="13">Trouser</option>
+              <option value="14">Groom Dress</option>
+            </optgroup>
+            <optgroup label="Casual wear">
+              <option value="15">Casual Wear blouse</option>
+              <option value="16">Casual wear pants</option>
+              <option value="17">Casual wear Denims</option>
+              <option value="18">Casual wearShorts</option>
+              <option value="19">Casual wear T-shirts</option>
+            </optgroup>
+          </select>
+        </div>
+
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Material" name="material">
+            </div>                             
+          </div>
+        </div>
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Color" name="color">
+            </div>                             
+          </div>
+        </div>
+      </div> 
+
+      <div class="d-flex">
+        <div class="f1">
+          <div class="measure-card-img">
+            <img src="/ucsc_2202_07/andum.lk/images/neck-01.png" class="body-img">
+          </div>
+          <div class="measure-card-value">
+            <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+            <input class="input-field" type="text" placeholder="Neck" name="neck">
+            <select class="option" name="Unit" id="unit" style="">
+              <option>in</option>
+              <option>cm</option>
+            </select>
+          </div>
+        </div>
+        <div class="f1">
+          <div class="measure-card-img">
+            <img src="/ucsc_2202_07/andum.lk/images/chest-01.png" class="body-img">
+          </div>
+          <div class="measure-card-value">
+            <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+            <input class="input-field" type="text" placeholder="Chest" name="chest">
+            <select class="option" name="Unit" id="unit" style="">
+              <option>in</option>
+              <option>cm</option>
+            </select>
+          </div>
+        </div>
+      </div>
+                        
+      <div class="d-flex">
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/waist-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Waist" name="waist">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/realhip-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Seat" name="seat">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex">
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/shirtl-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Shirt Length" name="shirtlength">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/back-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Shoulder Width" name="shoulderwidth">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex">
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/arm-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Arm Length" name="armlength">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/cuff-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Wrist" name="wrist">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex">
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/bicep-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Biceps" name="biceps">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+        <div class="f1">
+          <div class="measure-card">
+            <div class="measure-card-img">
+              <img src="/ucsc_2202_07/andum.lk/images/hip-01.png" class="body-img">
+            </div>
+            <div class="measure-card-value">
+              <!-- <lable for="neck" class="input-field" style="">Neck:</label> -->
+              <input class="input-field" type="text" placeholder="Hip" name="hip">
+              <select class="option" name="Unit" id="unit" style="">
+                <option>in</option>
+                <option>cm</option>
+              </select>
+            </div>                             
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex">
+        <!-- <lable for="other" style="">Other:</label> -->
+        <textarea name="message" rows="20" cols="50" placeholder="Other" class="txt-area"></textarea>
+      </div>
+      <center><button class="cart-button" type="submit" name="hireT">Submit Tailor Request</button></center>
+    </form>
+  </div>
+
+
   <?php } ?>
   <?php require("../footer.php") ?>
 </body>
