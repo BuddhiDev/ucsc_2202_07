f4fa57c7999b4f6f2e529095223f3101dfa614dd
Thashwini <thashwiniwattuhewa@gmail.com>
2020-11-13 15:08:52 +0530

    commited

--- andum.lk/fashion_designer/edit_profile.php
+++ andum.lk/fashion_designer/edit_profile.php
@@ -1,7 +1,3 @@
-<?php
-require("fd_controller.php");
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -63,143 +59,103 @@ require("fd_controller.php");
     })
   </script>
 
-  <?php
-    if(!empty($msg)):
-  ?>
 
-  <div>
-      <?php echo $msg;?>
-  </div>
-    <?php endif; ?> 
-
-<!-- edit profile form-->
-<div class="container-box">
   <form action="edit_profile.php" method="post" enctype="multipart/form-data">
-  <?php
+<!-- edit profile form-->
+  <div class="container-box">
+        <div class="fd-cover-box">
+          <div class="fd-row">
+            <h3 class="fd-edit-heading">Edit Profile</h3>
+          </div>
+          <div class="fd-form-container">
+            <div class="fd-form-container-block1">
+              <div class="profile-pic">
+                <div class="pic">
+                  <img src="/ucsc_2202_07/andum.lk/images/profile/photo1.png" id="profileDisplay" name="profileDisplay" onclick="triggerClick()" alt="" class="proDisp">
+                </div>        
+                <label for="profile_pictures"></label>
+                <input type="file" name="profilepic" value="" onchange="displayImage(this)" id="profilepic" class="fd-profile-pic-control">
+              </div>
+            </div>
+            <div class="fd-form-container-block2">
+              <div class="form-field-inline">
+                <label for="uname" class="field-label-inline">NIC Number</label>
+                <input type="text" value="" class="field-value-inline" name="nic" required>
+              </div>
+              <div class="form-field-inline">
+                <label for="email" class="field-label-inline"><b>Email Address</b></label>
+                <input type="text" value="" class="field-value-inline" name="email" required>
+              </div>
+              <div class="form-field-inline">
+                <label for="fname" class="field-label-inline"><b>First Name</b></label>
+                <input type="text" value="" class="field-value-inline" name="fname" required>
+              </div>
+              <div class="form-field-inline">
+                <label for="lname" class="field-label-inline"><b>Last Name</b></label>
+                <input type="text" value="" class="field-value-inline" name="lname" required>
+              </div>
+              <div class="form-field-inline">
+                <label for="password" class="field-label-inline"><b>Password</b></label>
+                <input type="password" class="field-value-inline" name="password" required>
+              </div>
+              <div class="form-field-inline">
+                <label for="cpassword" class="field-label-inline"><b>Confirm Password</b></label>
+                <input type="password" class="field-value-inline" name="cpassword" required>
+              </div>
+              <div class="form-field-inline">
+                <label for="contactno" class="field-label-inline"><b>Phone Number</b></label>
+                <input type="text" value="" class="field-value-inline" name="contactno">
+              </div>
+              <div class="form-field-inline">
+                <label for="address" class="field-label-inline"><b>Address</b></label>
+                <input type="text" value="" class="field-value-inline" name="address">
+              </div>
+              <div class="form-field-inline">
+                <label for="postalcode" class="field-label-inline"><b>Postal Code</b></label>
+                <input type="text" value="" class="field-value-inline" name="postalcode">
+              </div>
+            </div>
+            <div class="add-design-cover">
+              <div class="add-design-row">
+                <h3 class="fd-edit-heading">Add Designs</h3>
+              </div>
+              <div class="row">
+                <div class="col-4">
+                  <div class="add-design-pic">
+                    <div class="design-pic">
+                      <img src="/ucsc_2202_07/andum.lk/images/profile/photo.png" id="profileDisplay1" name="profileDisplay1" onclick="triggerClick1()" alt="" class="designDisp">
+                    </div>
+                    <input type="file" name="addpic1" value="" onchange="displayImage1(this)" id="addpic1" class="fd-profile-pic-control">
+                  </div>
+                </div>
+                <div class="col-4">
+                  <div class="add-design-pic">
+                    <div class="design-pic">
+                      <img src="/ucsc_2202_07/andum.lk/images/profile/photo.png" id="profileDisplay2" name="profileDisplay2" onclick="triggerClick2()" alt="" class="designDisp">
+                    </div>
+                    <input type="file" name="addpic2" value="" onchange="displayImage2(this)" id="addpic2" class="fd-profile-pic-control">
+                  </div>
+                </div>
+                <div class="col-4">
+                  <div class="add-design-pic">
+                    <div class="design-pic">
+                      <img src="/ucsc_2202_07/andum.lk/images/profile/photo.png" id="profileDisplay3" name="profileDisplay3" onclick="triggerClick3()" alt="" class="designDisp">
+                    </div>
+                    <input type="file" name="addpic3" value="" onchange="displayImage3(this)" id="addpic3" class="fd-profile-pic-control">
+                  </div>
+                </div>
+                <div class="add-design-row">
+                  <button class="loginbutton btn-full-w" type="submit" name="upload_design">Upload</button>
+                </div>
+              </div>
 
-    include("../errors.php");
-
-
-    $nic = $_SESSION['nic'];
-    $sql= "SELECT * FROM users WHERE nic=$nic";
-    // $sql = "SELECT nic, email,fname,lname,password,contactno FROM users";
-    $result = mysqli_query($db, $sql);
-
-    if ($result) {
-      while ($row = mysqli_fetch_assoc($result)) {
-  ?>
-
-    <div class="fd-cover-box">
-      <div class="fd-row">
-        <h3 class="fd-edit-heading">Edit Profile</h3>
-      </div>
-      <div class="fd-form-container">
-        <div class="fd-form-container-block1">
-          <div class="profile-pic">
-            <div class="pic">
-              <img src="/ucsc_2202_07/andum.lk/images/profile/photo1.png" id="profileDisplay" name="profileDisplay" onclick="triggerClick()" alt="" class="proDisp">
-            </div>        
-            <label for="profile_pictures"></label>
-            <input type="file" name="profilepic" value="<?php echo $row["image"]?>" onchange="displayImage(this)" id="profilepic" class="fd-profile-pic-control">
+            </div>
+            <div class="btn-panel-center">
+              <button class="loginbutton btn-full-w" type="submit" name="update_fd">Save Data</button>
+            </div>
           </div>
         </div>
-        <div class="fd-form-container-block2">
-          <div class="form-field-inline">
-            <label for="uname" class="field-label-inline">NIC Number</label>
-            <input type="text" value="<?php echo $row["nic"]?>" class="field-value-inline" name="nic" required>
-          </div>
-          <div class="form-field-inline">
-            <label for="email" class="field-label-inline"><b>Email Address</b></label>
-            <input type="text" value="<?php echo $row["fname"]?>" class="field-value-inline" name="email" required>
-          </div>
-          <div class="form-field-inline">
-            <label for="fname" class="field-label-inline"><b>First Name</b></label>
-            <input type="text" value="<?php echo $row["lname"]?>" class="field-value-inline" name="fname" required>
-          </div>
-          <div class="form-field-inline">
-            <label for="lname" class="field-label-inline"><b>Last Name</b></label>
-            <input type="text" value="" class="field-value-inline" name="lname" required>
-          </div>
-          <div class="form-field-inline">
-            <label for="password" class="field-label-inline"><b>Password</b></label>
-            <input type="password" class="field-value-inline" name="password" required>
-          </div>
-          <div class="form-field-inline">
-            <label for="cpassword" class="field-label-inline"><b>Confirm Password</b></label>
-            <input type="password" class="field-value-inline" name="cpassword" required>
-          </div>
-          <div class="form-field-inline">
-            <label for="contactno" class="field-label-inline"><b>Phone Number</b></label>
-            <input type="text" value="<?php echo $row["contactno"]?>" class="field-value-inline" name="contactno">
-          </div>
-          <div class="form-field-inline">
-            <label for="address" class="field-label-inline"><b>Address</b></label>
-            <input type="text" value="<?php echo $row["address"]?>" class="field-value-inline" name="address">
-          </div>
-          <div class="form-field-inline">
-            <label for="postalcode" class="field-label-inline"><b>Postal Code</b></label>
-            <input type="text" value="<?php echo $row["postalcode"]?>" class="field-value-inline" name="postalcode">
-          </div>
         
-          
-          
-          
-          <?php
-            }
-          }
-          ?>
-        </div>
-        <div class="add-design-cover">
-          <div class="add-design-row">
-            <h3 class="fd-edit-heading">Add Designs</h3>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <div class="add-design-pic">
-                <div class="design-pic">
-                  <img src="/ucsc_2202_07/andum.lk/images/profile/photo.png" id="profileDisplay1" name="profileDisplay1" onclick="triggerClick1()" alt="" class="designDisp">
-                </div>
-                <input type="file" name="addpic1" value="<?php echo $row["image"]?>" onchange="displayImage1(this)" id="addpic1" class="fd-profile-pic-control">
-              </div>
-            </div>
-            <div class="col-4">
-              <div class="add-design-pic">
-                <div class="design-pic">
-                  <img src="/ucsc_2202_07/andum.lk/images/profile/photo.png" id="profileDisplay2" name="profileDisplay2" onclick="triggerClick2()" alt="" class="designDisp">
-                </div>
-                <input type="file" name="addpic2" value="<?php echo $row["image"]?>" onchange="displayImage2(this)" id="addpic2" class="fd-profile-pic-control">
-              </div>
-            </div>
-            <div class="col-4">
-              <div class="add-design-pic">
-                <div class="design-pic">
-                  <img src="/ucsc_2202_07/andum.lk/images/profile/photo.png" id="profileDisplay3" name="profileDisplay3" onclick="triggerClick3()" alt="" class="designDisp">
-                </div>
-                <input type="file" name="addpic3" value="<?php echo $row["image"]?>" onchange="displayImage3(this)" id="addpic3" class="fd-profile-pic-control">
-              </div>
-            </div>
-            <div class="add-design-row">
-              <button class="loginbutton btn-full-w" type="submit" name="upload_design">Upload</button>
-            </div>
-            <!-- <label for="postalcode" class="field-label-inline"><b>Upload Design</b></label>
-            <input type="hidden" name="size" id="" value="1000000">
-            <div>
-              <input type="file" name="image">
-            </div>
-            <div>
-              <textarea name="text" id="" cols="40" rows="4" placeholder="Design Discription"></textarea>
-            </div>
-            <div>
-              <input type="submit" name="uploaddesign" value="Upload Design">
-            </div> -->
-          </div>
-
-        </div>
-        <div class="btn-panel-center">
-            <button class="loginbutton btn-full-w" type="submit" name="update_fd">Save Data</button>
-          </div>
-      </div>
-    </div>
   </form>
   
   
