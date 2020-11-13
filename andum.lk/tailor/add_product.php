c666d21c8d9c46efb949b8528775c81c4dda3f16
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2020-11-13 13:51:00 +0530

    Update add_product.php

--- andum.lk/tailor/add_product.php
+++ andum.lk/tailor/add_product.php
@@ -9,9 +9,7 @@ require("../server.php");
   <link rel="shortcut icon" href="logo.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/style.css">
-  <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/loginstyle.css">
-  <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/tailorstyle.css">
-  <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/fashion-designer.css">
+<link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/loginstyle.css">
 <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/checkbox.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://kit.fontawesome.com/dc4ee3e80e.js" crossorigin="anonymous"></script>
@@ -19,124 +17,94 @@ require("../server.php");
 </head>
 
 <body>
-  <header>
+<header>
     <nav class="navbar-main">
       <div class="navbar-logo">
         <img class="logo" src="../logo.png" alt="logo" class="img-box">
       </div>
       <div class="nav-item-middle">
         <ul class="nav-area">
-          <li><a href="tailor-dashboard.php">Home</a></li>
+          <li><a href="index.php">Home</a></li>
           <li class="dropdown">
             <a href="#">Women</a>
-            <div class="row">
-              <ul class="menu-area">
-                <div class="col-4">
-                  <ul class="inner-menu">
-                    <li class="inner-list-header">Casual Wear</li>
-                    <li><a href="#">Dresses</a></li>
-                    <li><a href="#">T shirts</a></li>
-                    <li><a href="#">Blouse</a></li>
-                    <li><a href="#">Shirts</a></li>
-                    <li><a href="#">Jeans</a></li>
-                    <li><a href="#">Pants</a></li>
-                  </ul>
-                </div>
-                <div class="col-4">
-                  <ul class="inner-menu">
-                    <li class="inner-list-header">Formal Wear</li>
-                    <li><a href="#">Dresses</a></li>
-                    <li><a href="#">Blouse</a></li>
-                    <li><a href="#">Skirts</a></li>
-                    <li><a href="workwear_gents.php">Pants</a></li>
-                  </ul>
-                </div>
-                <div class="col-4">
-                  <ul class="inner-menu">
-                    <li class="inner-list-header">Ethnic wear</li>
-                    <li><a href="#">Kurthas</a></li>
-                    <li><a href="#">Shlwas</a></li>
-                    <li><a href="#">Sarees</a></li>
-                  </ul>
-                </div>
+            <ul class="menu-area">
+              <ul class="inner-menu">
+                <li class="inner-list-header">Casual Wear</li>
+                <li><a href="#">T shirts</a></li>
+                <!-- <li><a href="workware_ladies.php">Blouse</a></li> -->
+                <!-- <li><a href="#">Shirts</a></li>
+                <li><a href="#">Jeans</a></li>
+                <li><a href="#">Pants</a></li>
+                <li><a href="dresses.php">Dresses</a></li> -->
               </ul>
-            </div>
-              
-            
+              <ul class="inner-menu">
+                <li class="inner-list-header">Formal Wear</li>
+                <li><a href="#">Blouse</a></li>
+                <li><a href="#">Skirts</a></li>
+                <li><a href="workwear_gents.php">Pants</a></li>
+                <li><a href="#">Dresses</a></li>
+              </ul>
+              <ul class="inner-menu">
+                <li class="inner-list-header">Ethnic wear</li>
+                <li><a href="#">Kurthas</a></li>
+                <li><a href="#">Shawls</a></li>
+                <li><a href="#">Sarees</a></li>
+              </ul>
+            </ul>
           </li>
           <li class="dropdown">
             <a href="#">Men</a>
-            <div class="row">
-              <ul class="menu-area">
-                <div class="col-6">
-                  <ul class="inner-menu">
-                    <li class="inner-list-header">Casual Wear</li>
-                    <li><a href="#">T shirts</a></li>
-                    <li><a href="#">Shirts</a></li>
-                    <li><a href="#">Jeans</a></li>
-                    <li><a href="index.php?dcategory=trouser">Trousers</a></li>
-                    <li><a href="#">Sarong</a></li>
-                    <li><a href="#">Shorts</a></li>
-                  </ul>  
-                </div>
-                <div class="col-6">
-                  <ul class="inner-menu">
-                    <li class="inner-list-header">Formal Wear</li>
-                    <li><a href="#">Shirts</a></li>
-                    <li><a href="#">Trousers</a></li>
-                    <li><a href="#">Blazers</a></li>
-                  </ul>
-                </div> 
+            <ul class="menu-area">
+              <ul class="inner-menu">
+                <li class="inner-list-header">Casual Wear</li>
+                <li><a href="#">T shirts</a></li>
+                <li><a href="#">Shirts</a></li>
+                <li><a href="#">Jeans</a></li>
+                <li><a href="#">Trousers</a></li>
+                <li><a href="#">Bottoms</a></li>
+                <li><a href="#">Sarongs</a></li>
               </ul>
-            </div>            
+              <ul class="inner-menu">
+                <li class="inner-list-header">Formal Wear</li>
+                <li><a href="#">Shirts</a></li>
+                <li><a href="#">Trousers</a></li>
+                <li><a href="#">Blazers</a></li>
+              </ul>
+              <ul class="inner-menu">
+                <li class="inner-list-header">Inner Wear</li>
+                <li><a href="#">Brief</a></li>
+                <li><a href="#">Trunks</a></li>
+                <li><a href="#">Vests</a></li>
+              </ul>
+            </ul>
           </li>
           <li class="dropdown">
             <a href="#">Kids</a>
-            <div class="row">
             <ul class="menu-area">
-              <div class=col-6>
-                <ul class="inner-menu">
-                  <li class="inner-list-header">Boys</li>
-                  <li><a href="#">T shirts</a></li>
-                  <li><a href="#">Tank Tops</a></li>
-                  <li><a href="#">Shirts</a></li>
-                  <li><a href="#">Shorts</a></li>
-                  <li><a href="#">Pants</a></li>
-                  <li><a href="#">Sleepware</a></li>
-                </ul>
-              </div>
-              <div class="col-6">
-                <ul class="inner-menu">
-                  <li class="inner-list-header">Girls</li>
-                  <li><a href="#">T shirts</a></li>
-                  <li><a href="#">Dresses</a></li>
-                  <li><a href="#">Shorts</a></li>
-                  <li><a href="#">Sleepware</a></li>
-                </ul>
-              </div>             
+              <ul class="inner-menu">
+                <li class="inner-list-header">Girls</li>
+                <li><a href="#">T shirts</a></li>
+                <li><a href="#">Tank Tops</a></li>
+                <li><a href="#">Shirts</a></li>
+                <li><a href="#">Shorts</a></li>
+                <li><a href="#">Pants</a></li>
+                <li><a href="#">Sleepware</a></li>
+              </ul>
+              <ul class="inner-menu">
+                <li class="inner-list-header">Boys</li>
+                <li><a href="#">T shirts</a></li>
+                <li><a href="#">Dresses</a></li>
+                <li><a href="#">Kurta</a></li>
+                <li><a href="#">Shorts</a></li>
+                <li><a href="#">Sleepware</a></li>
+              </ul>
             </ul>
-              
-
-            </div>
-            
           </li>
 
-          <li><a href="hire_tailor.php">Explore Tailors</a></li>
-          <li><a href="hire_fashion_designer.php">Explore Fashion Designers</a></li>
-          <li>
-          <div class="search-container">
-        <form method="post">
-          <div class="form-field-inline">
-            <input type="text" class="field-value-inline" name="q" placeholder="Search...">
-            <button type="submit" class="search-input-group-btn" name="search"><i class="fa fa-search" aria-hidden="true"></i></button>
-          </div>
-        </form>
-      </div>
-          </li>
+          <li><a href="add_product.php">Add a New Dress</a></li>
+          
         </ul>
       </div>
-      
-      <!-- </div> -->
       <div class="box">
         <div class="nav_right">
           <ul>
@@ -144,15 +112,14 @@ require("../server.php");
               <div class="dd_right">
                 <ul>
                   <li><a href="edit_profile.php"><i class="fas fa-edit"></i>Edit Profile</a></li>
-                  <li><a href="index.php"><i class="fas fa-chart-line"></i>Dashboard</a></li>
-                  <li><a href="fd-manage-orders.php"><i class="fas fa-money"></i>Purchases</a></li>
+                  <li><a href="Manage_order.php"><i class="fas fa-users"></i>Customer Orders</a></li>
+                  <li><a href="purchases.php"><i class="fas fa-money"></i>Sales</a></li>
                   <li><a href="#"><i class="fas fa-heart"></i>Favourites</a></li>
                   <li><a href="index.php?logout='1'"><i class="fas fa-sign-out-alt" name="logout"></i>Sign Out</a></li>
                 </ul>
-              </div>
-            </li>
-            <li><i class="fas fa-envelope"></i></li>
-            <li><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
+
+                <li><i class="fas fa-envelope"></i></li>
+              </div> 
           </ul>
         </div>
       </div>
@@ -260,23 +227,23 @@ require("../server.php");
       <div class="col-75">
       <!--  <div class="column">-->
         <label class="checkbox_container">Red
-          <input type="checkbox" name="color[]" value="#ff0000">
+          <input type="checkbox" name="color[]" value="red">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">Orange
-          <input type="checkbox" name="color[]" value="#000000">
+          <input type="checkbox" name="color[]" value="orange">
           <span class="checkmark"></span>
         </label>
-        <label class="checkbox_container">Yelllow
-          <input type="checkbox" name="color[]" value="#ffffff">
+        <label class="checkbox_container">Yellow
+          <input type="checkbox" name="color[]" value="yellow">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">Green
-          <input type="checkbox" name="color[]" value="#0000ff">
+          <input type="checkbox" name="color[]" value="green">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">Blue
-          <input type="checkbox" name="color[]" value="#ffc0cb">
+          <input type="checkbox" name="color[]" value="blue">
           <span class="checkmark"></span>
         </label>
       <!--  </div>
@@ -284,23 +251,23 @@ require("../server.php");
         
         
         <label class="checkbox_container">Purple
-          <input type="checkbox" name="color[]" value="#">
+          <input type="checkbox" name="color[]" value="purple">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">Pink
-          <input type="checkbox" name="color[]" value="#">
+          <input type="checkbox" name="color[]" value="pink">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">Black
-          <input type="checkbox" name="color[]" value="#">
+          <input type="checkbox" name="color[]" value="black">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">White
-          <input type="checkbox" name="color[]" value="#ffc0cb">
+          <input type="checkbox" name="color[]" value="white">
           <span class="checkmark"></span>
         </label>
         <label class="checkbox_container">Other
-          <input type="checkbox" name="color[]" value="#">
+          <input type="checkbox" name="color[]" value="other">
           <span class="checkmark"></span>
         </label>
       <!--  </div>-->
