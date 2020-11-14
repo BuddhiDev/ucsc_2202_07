e34d95817fe5368c135ef87fe1809f1d613b7164
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-11-14 19:11:13 +0530

    ss

--- andum.lk/fashion_designer/edit_profile.php
+++ andum.lk/fashion_designer/edit_profile.php
@@ -3,6 +3,104 @@
 require("../server.php");
 ?>
 
+<?php
+
+if (isset($_POST['update_fd'])) {
+
+  $f_id =  $_SESSION['nic'];
+
+  $nic = mysqli_real_escape_string($db, $_POST['nic']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $fname = mysqli_real_escape_string($db, $_POST['fname']);
+  $lname = mysqli_real_escape_string($db, $_POST['lname']);
+  $password1 = mysqli_real_escape_string($db, $_POST['password']);
+  $password2 = mysqli_real_escape_string($db, $_POST['cpassword']);
+  $contactno = mysqli_real_escape_string($db, $_POST['contactno']);
+  $address = mysqli_real_escape_string($db, $_POST['address']);
+  $postalcode = mysqli_real_escape_string($db, $_POST['postalcode']);
+  
+  $filename = $_FILES["profilepic"]["name"];
+  $tempname = $_FILES["profilepic"]["tmp_name"];
+  $folder = "profile_pictures/".$filename;
+
+  if ($password1 != $password2){
+      array_push($errors, "Passwords do not match");
+  }
+  else{
+      $password = md5($password1);
+      $sql = "UPDATE users SET fname='$fname',lname='$lname',contactno='$contactno',password='$password',address='$address',postalcode='$postalcode',image ='$filename' WHERE nic='$nic'";
+      $result=mysqli_query($db, $sql);
+
+      if (move_uploaded_file($tempname, $folder))
+      {
+          echo "<script>alert('Image Has Been Uploaded')</script>";
+      }
+      else
+      {
+          echo "<script>alert('Image Does Not Uploaded')</script>";
+      }
+
+  }
+
+  $filename1 = $_FILES["addpic1"]["name"];
+  $tempname1 = $_FILES["addpic1"]["tmp_name"];
+  $folder1 = "profile_pictures/".$filename1;
+
+      $sql1 = "INSERT INTO fashion_designer (image1) VALUES ('$filename1') WHERE nic='$nic'";
+      $result1=mysqli_query($db, $sql1);
+
+      if (move_uploaded_file($tempname1, $folder1))
+      {
+          echo "<script>alert('Image Has Been Uploaded')</script>";
+      }
+      else
+      {
+          echo "<script>alert('Image Does Not Uploaded')</script>";
+      }
+
+     //design2
+      $filename2 = $_FILES["addpic2"]["name"];
+      $tempname2 = $_FILES["addpic2"]["tmp_name"];
+      $folder2 = "profile_pictures/".$filename2;
+  
+     
+     
+      $sql2 = "INSERT INTO fashion_designer (image2) VALUES ('$filename2') WHERE nic='$nic' ";
+          $result2=mysqli_query($db, $sql2);
+  
+          if (move_uploaded_file($tempname2, $folder2))
+          {
+              echo "<script>alert('Image Has Been Uploaded')</script>";
+          }
+          else
+          {
+              echo "<script>alert('Image Does Not Uploaded')</script>";
+          }
+
+
+          //design3
+
+          $filename3 = $_FILES["addpic3"]["name"];
+          $tempname3 = $_FILES["addpic3"]["tmp_name"];
+          $folder3 = "profile_pictures/".$filename3;
+      
+          $sql3 = "INSERT INTO fashion_designer (image3) VALUES ('$filename3') WHERE nic='$nic' ";
+          $result3=mysqli_query($db, $sql3);
+  
+         
+              if (move_uploaded_file($tempname3, $folder3))
+              {
+                  echo "<script>alert('Image Has Been Uploaded')</script>";
+              }
+              else
+              {
+                  echo "<script>alert('Image Does Not Uploaded')</script>";
+              }
+
+
+}
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -287,6 +385,7 @@ require("../server.php");
                 <!-- <div class="add-design-row">
                   <button class="loginbutton btn-full-w" type="submit" name="upload_design">Upload</button>
                 </div> -->
+
               </div>
     
 
@@ -313,7 +412,7 @@ require("../server.php");
           <div class="btn-panel-center">
             <!-- <button class="facebookbutton btn-full-w" type="submit">SIGN UP WITH FACEBOOK </button>
             <button class="googlebutton btn-full-w" type="submit">SIGN UP WITH GOOGLE </button> -->
-            <button class="loginbutton btn-full-w" type="submit" name="update_user">Save Data</button>
+            <button class="loginbutton btn-full-w" type="submit" name="update_fd">Save Data</button>
           </div>
 
           <?php
