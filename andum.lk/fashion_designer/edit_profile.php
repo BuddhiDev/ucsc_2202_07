1bdcb04e18e40794141287ff1bfbed5010c8735e
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2020-11-14 19:31:24 +0530

    Update edit_profile.php

--- andum.lk/fashion_designer/edit_profile.php
+++ andum.lk/fashion_designer/edit_profile.php
@@ -46,7 +46,7 @@ if (isset($_POST['update_fd'])) {
   $tempname1 = $_FILES["addpic1"]["tmp_name"];
   $folder1 = "profile_pictures/".$filename1;
 
-      $sql1 = "INSERT INTO fashion_designer (image1) VALUES ('$filename1') WHERE nic='$nic'";
+      $sql1 = "UPDATE fashion_designer SET  image1='$filename1' WHERE nic='$nic'";
       $result1=mysqli_query($db, $sql1);
 
       if (move_uploaded_file($tempname1, $folder1))
@@ -65,7 +65,7 @@ if (isset($_POST['update_fd'])) {
   
      
      
-      $sql2 = "INSERT INTO fashion_designer (image2) VALUES ('$filename2') WHERE nic='$nic' ";
+      $sql2 = "UPDATE fashion_designer SET  image2='$filename2' WHERE nic='$nic'";
           $result2=mysqli_query($db, $sql2);
   
           if (move_uploaded_file($tempname2, $folder2))
@@ -84,7 +84,7 @@ if (isset($_POST['update_fd'])) {
           $tempname3 = $_FILES["addpic3"]["tmp_name"];
           $folder3 = "profile_pictures/".$filename3;
       
-          $sql3 = "INSERT INTO fashion_designer (image3) VALUES ('$filename3') WHERE nic='$nic' ";
+          $sql3 = "UPDATE fashion_designer SET  image3='$filename3' WHERE nic='$nic'";
           $result3=mysqli_query($db, $sql3);
   
          
