2e9b7bf56d535aa1a86d4f6075eac7bac8be87b2
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-01-11 22:54:26 +0530

    df

--- andum.lk/tailor/my_showcase.php
+++ andum.lk/tailor/my_showcase.php
@@ -86,7 +86,7 @@ if (!isset($_SESSION['nic'])) {
 
           <div class="card-item">
             <div class="card-img">
-            <a href="my_showcase.php?edit_dress_id=<?php echo $row["dress_id"] ?>"><img src="/ucsc_2202_07/andum.lk/tailor/products/<?php echo $row["image"]; ?> " alt="Avatar" style="width:100%">
+            <a href="my_showcase.php?view_dress_id=<?php echo $row["dress_id"] ?>"><img src="/ucsc_2202_07/andum.lk/tailor/products/<?php echo $row["image"]; ?> " alt="Avatar" style="width:100%">
             </div>
             <div class="card-content">
               <div class="card-title"><?php echo $row["title"] ?></div>
@@ -159,10 +159,6 @@ if (!isset($_SESSION['nic'])) {
             <div class="card-content">
               <div class="card-title"><?php echo $row["title"] ?></div>
               <div class="card-description">LKR <?php echo $row["price"]?>.00</div>
-              <div>
-                <a href="my_showcase.php?edit_dress_id=<?php echo $row["dress_id"]?>"><button class="loginbutton btn-full-w">EDIT</button></a>
-                <a href="my_showcase.php?rem_dress_id=<?php echo $row["dress_id"]?>"><button class="loginbutton btn-full-w">REMOVE</button></a>
-              </div>
             </div>
           </div>
         </form>
