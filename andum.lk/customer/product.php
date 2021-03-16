2aa12a5eeab8046dfec4ecdf7afa4b06d08b92c0
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-03-16 19:05:25 +0530

    Update product.php

--- andum.lk/customer/product.php
+++ andum.lk/customer/product.php
@@ -279,7 +279,6 @@ if (!isset($_SESSION['nic'])) {
               <div class="customized">
                 <input type="hidden" value="<?php echo $row["dress_id"] ?> " name="dress_id">
                 <input type="hidden" value="<?php echo $nic ?> " name="c_nic">
-                <!--<input name="size" class="option" value="<?php echo $new_size ?>">-->
                 <button class="cart-button" type="submit" name="addTocart" <?php if($item_added=="true") echo "disabled"?>><?php if($item_added=="false"){?> ADD TO CART<?php } else {?>ADDED TO CART<?php }?></button>
               </div>
               <div class="customized">
