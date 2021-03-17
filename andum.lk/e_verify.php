dbdd141b792d0121d6790a52a082b091a9c690d3
Nimeshika98 <59416698+Nimeshika98@users.noreply.github.com>
2021-03-17 23:43:06 +0530

    v

--- andum.lk/e_verify.php
+++ andum.lk/e_verify.php
@@ -7,17 +7,17 @@
 if(isset($_GET['vkey'])){
     //process verification
     $vkey = $_GET['vkey'];
-
-    $sqlresultset = "SELECT * FROM users WHERE verified = 0 AND vkey = '$vkey' LIMIT 1 ";
+    $selected_vnic = $_SESSION['verify_nic'];
+    $sqlresultset = "SELECT * FROM users WHERE verified=0 AND vkey='$vkey' LIMIT 1 ";
     $resultset = mysqli_query($db, $sqlresultset);
 
     if ($resultset) {
       while($row = mysqli_fetch_assoc($resultset)){
 
         //validate the email
-
-        $update_vkey="UPDATE users SET verified = 1 where vkey = '$vkey' LIMIT 1 ";
-        $update_res= mysqli_query($db,$update_vkey );
+        
+        $update_vkey="UPDATE users SET verified=1 where nic='$selected_vnic' ";
+        $update_res= mysqli_query($db,$update_vkey);
 
         if ($update_res)
         {
