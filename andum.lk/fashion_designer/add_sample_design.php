72cf66ff5d4183225fddb1d8a0cf8d02f1f21926
Viveka Hewapathirana <57043436+viveka97@users.noreply.github.com>
2021-03-24 11:14:20 +0530

    s

--- andum.lk/fashion_designer/add_sample_design.php
+++ andum.lk/fashion_designer/add_sample_design.php
@@ -5,7 +5,7 @@ require("fd_controller.php");
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Andum.lk - Add a dress</title>
+  <title>Andum.lk - Add a Design</title>
   <link rel="shortcut icon" href="logo.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/ucsc_2202_07/andum.lk/styles/style.css">
